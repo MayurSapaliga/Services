db12bb996202398bea15b01bd5f4aac3435d2fb6
Mayur Sapaliga <msapaliga@collabware.com>
2023-02-03 11:52:56 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Services
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public string PrintServices()
+        {
+            string message = "I'm Base Service";
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
